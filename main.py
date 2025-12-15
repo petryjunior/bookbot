073552b0dd835b7f0aba1ac6a503f798d4ece406
petryjunior <petryjunior@gmail.com>
2025-12-15 17:04:25 -0300

    Bookbot 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import number_of_words
 from stats import count_characters
+from stats import sorted_dictionaries
 
 def get_book_text(path):
     with open(path) as f:
@@ -10,8 +11,18 @@ def main():
     path = "books/frankenstein.txt"
     result = get_book_text(path)
     chars = count_characters(result)
+    sorted_char_list = sorted_dictionaries(chars)
     num_words = number_of_words(result)
+    print("============ BOOKBOT ============")
+    print (f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
     print("Found", num_words, "total words")
-    print(chars)
+    print("--------- Character Count -------")
+    for item in sorted_char_list:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+        else:
+            continue
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -13,4 +13,17 @@ def count_characters(text):
             char_counts[ch] = 1
     return char_counts
 
+def sorted_dictionaries(char_counts):
+    my_list = []
+    for char, num in char_counts.items():
+        characters = {"char": char, "num": num}
+        my_list.append(characters)
+    my_list.sort(reverse=True, key=sort_on)
+    return my_list
+
+def sort_on(items):
+    return items["num"]
+
+        
+
 
